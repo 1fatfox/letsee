a7073457668e5eb62dfba0ce1f486ab06e0869d4
Emily CJones <emilycjones@college.harvard.edu>
2018-11-13 17:45:11 -0500

    add initial app component

---
+++ frontend/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+const App = () => {
+  <div>
+  </div>;
+};
