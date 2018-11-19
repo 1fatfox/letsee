2acf34e4e320d04c80d8b3cbe6c7cdeb0621a44f
EmilyCJones <emilycjones@college.harvard.edu>
2018-11-19 15:09:12 -0500

    try to seed with pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,6 @@ u3 = User.create!({email: "test3@email.com", password:"test123", first_name: "te
 s1 = Shop.create!({ name: "Shop1", user_id: u1.id})
 s2 = Shop.create!({ name: "Shop2", user_id: u2.id})
 s3 = Shop.create!({ name: "Shop3", user_id: u3.id})
-p1 = Product.create!({title: "test", description: "Product1", image_url: "abc", price: "12" , shop_id: s1.id})
+p1 = Product.create!({title: "test", description: "Product1", image_url: "products/copper_birdcage.jpg", price: "12" , shop_id: s1.id})
 p2 = Product.create!({title: "test2", description: "Product2", image_url: "abcd", price: "13" , shop_id: s2.id})
 p3 = Product.create!({title: "test3", description: "Product3", image_url: "abcde", price: "14" , shop_id: s3.id})
