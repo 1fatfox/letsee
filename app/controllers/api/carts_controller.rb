1b00aa2bae50bece800d506b955acf587f960054
EmilyCJones <emilycjones@college.harvard.edu>
2018-11-22 17:12:14 -0500

    update cart controller

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,3 +1,23 @@
 class Api::CartController < ApplicationController
 
+  def create
+    @cart = Cart.new(cart_params)
+    if @cart.save
+      render :show
+    else
+      render json: @cart.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @cart = Cart.find_by(id: params[:id])
+  end
+
+
+
+  def cart_params
+    params.require(:cart).permit(:user_id)
+  end
+
+
 end
