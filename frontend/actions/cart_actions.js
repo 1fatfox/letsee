a9de6da126999b2322f5a8157c9944b7c98e72e9
EmilyCJones <emilycjones@college.harvard.edu>
2018-11-22 17:56:58 -0500

    cart actions

---
+++ frontend/actions/cart_actions.js
@@ -0,0 +1,3 @@
+import * as ApiUtil from '../util/cart_api_util';
+
+export const RECEIVE_CART = 'RECEIVE_CART';
---
+++ frontend/actions/cart_product_actions.js
@@ -0,0 +1,4 @@
+import * as ApiUtil from '../util/cart_product_api_util';
+
+export const RECEIVE_CART_PRODUCT = 'RECEIVE_CART_PRODUCT';
+export const REMOVE_CART_PRODUCT = 'REMOVE_CART_PRODUCT';
