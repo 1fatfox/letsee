2710439fccf9d9d2564b536442ee0d1b1a0c8445
EmilyCJones <emilycjones@college.harvard.edu>
2018-11-23 14:48:55 -0500

    add actions to cart products

--- frontend/actions/cart_product_actions.js
+++ frontend/actions/cart_product_actions.js
@@ -2,3 +2,18 @@ import * as ApiUtil from '../util/cart_product_api_util';
 
 export const RECEIVE_CART_PRODUCT = 'RECEIVE_CART_PRODUCT';
 export const REMOVE_CART_PRODUCT = 'REMOVE_CART_PRODUCT';
+
+
+export const receiveCartProducts = (cart_products) => {
+  return {
+    type: RECEIVE_CART_PRODUCTS,
+    products
+  };
+};
+
+export const receiveCartProduct = (cart_product) => {
+  return {
+    type: RECEIVE_CART_PRODUCT,
+    product
+  };
+};
