bdd912603c205975dcd37171c5634b18f83dd088
EmilyCJones <emilycjones@college.harvard.edu>
2018-11-23 14:57:23 -0500

    udpate cart product actions

--- frontend/actions/cart_product_actions.js
+++ frontend/actions/cart_product_actions.js
@@ -17,3 +17,55 @@ export const receiveCartProduct = (cart_product) => {
     product
   };
 };
+
+export const removeCartProduct = (id) => {
+  return {
+    type: REMOVE__CART_PRODUCT,
+    id
+  };
+};
+
+xport const receiveCartProductErrors = (errors) =>({
+    type: RECEIVE_CART_PRODUCT_ERRORS,
+    errors
+});
+
+export const clearCartProductErrors = () => ({
+  type: CLEAR_CART_PRODUCT_ERRORS
+});
+
+
+export const fetchCartProducts = (cart) => dispatch => {
+  return APIUtil.fetchCartProducts(cart).then(
+    cartProducts => dispatch(receiveCartProducts(cart_products)),
+    err => dispatch(receiveProductErrors(err.responseJSON))
+  );
+};
+
+export const fetchProduct = (cartProduct) => dispatch => {
+  return APIUtil.fetchCartProduct(id).then(
+    cartProduct => dispatch(receiveProduct(cartProduct)),
+    error => dispatch(receiveProductErrors(error.responseJSON))
+  );
+};
+
+export const createProduct = (product) => dispatch => {
+  return API.createProduct(product).then(
+    createdProduct => dispatch(receiveProduct(createdProduct)),
+    error => dispatch(receiveProductErrors(error.responseJSON))
+  );
+};
+
+export const updateProduct = (product) => dispatch => {
+  return API.updateProduct(product).then(
+    updatedProduct => dispatch(receiveProduct(updatedProduct)),
+    error => dispatch(receiveProductErrors(error.responseJSON))
+  );
+};
+
+export const deleteProduct = (productId) => dispatch => {
+  return API.deleteProduct(productId).then(
+    product => dispatch(removeProduct(productId)),
+    error => dispatch(receiveProductErrors(error.responseJSON))
+  );
+};
